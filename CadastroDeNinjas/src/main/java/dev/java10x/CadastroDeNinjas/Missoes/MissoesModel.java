5a7860a536648b8e51fcbe9fe9aa8a7caa3624cb
Adson <adsonchristopher@gmail.com>
2025-11-24 00:13:28 -0300

    feat: Coloca o lombok na classe para abstração

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,10 +2,21 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.LinkedList;
 
 @Entity
 @Table(name ="tb_missoes")
+
+//putaria cria automaticamente todos construtores caralhoosss
+@NoArgsConstructor
+@AllArgsConstructor
+//cria os getters e setters essa porra
+@Data
+
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,38 +28,6 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     private LinkedList<NinjaModel> ninjas = new LinkedList<>();
     private boolean status;
-
-    MissoesModel(){}
-
-    public MissoesModel(String missaoNome, Niveis nivel) {
-        this.missaoNome = missaoNome;
-        this.nivel = nivel;
-    }
-    public MissoesModel(long id, String missaoNome, Niveis nivel) {
-        this(missaoNome, nivel);
-        this.id = id;
-    }
-    public MissoesModel(long id, String missaoNome, Niveis nivel, LinkedList<NinjaModel> ninjas, boolean status) {
-        this(id, missaoNome, nivel);
-        this.ninjas = ninjas;
-        this.status = status;
-    }
-
-    public String getMissaoNome() {
-        return missaoNome;
-    }
-
-    public void setMissaoNome(String missaoNome) {
-        this.missaoNome = missaoNome;
-    }
-
-    public Niveis getNivel() {
-        return nivel;
-    }
-
-    public void setNivel(Niveis nivel) {
-        this.nivel = nivel;
-    }
 
     @Override
     public String toString() {
--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,12 +1,18 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 //entity trasnforma uma classe em uma entidade do db
 @Entity
 @Table(name = "tb_cadastro")
-
+//LOMBOK
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel  {
     //necessario ter um id
     // java declara id automaticamente
@@ -22,48 +28,4 @@ public class NinjaModel  {
     private MissoesModel missoes;
     //Many to one um ninja tem uma unica missao
 
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-
-    }
-
-    public NinjaModel(String nome,int idade, String email, Long id) {
-        this(nome, email, idade);
-        this.id = id;
-    }
-
-    public NinjaModel(String nome, int idade, String email, long id, MissoesModel missoes) {
-        this(nome, idade, email, id);
-        this.missoes = missoes;
-    }
 }

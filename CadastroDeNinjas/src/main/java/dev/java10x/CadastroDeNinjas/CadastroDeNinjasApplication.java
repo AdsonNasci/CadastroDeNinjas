10cf6b17c01259fdf4e5b234b61b6e2549d54994
Adson <adsonchristopher@gmail.com>
2025-11-28 23:11:46 -0300

    Fix : Adding propeties to the db

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -2,7 +2,7 @@ package dev.java10x.CadastroDeNinjas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+
 
 @SpringBootApplication
 public class CadastroDeNinjasApplication {
